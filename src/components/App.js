36854128c55f76202d16fcdc22f9c4288c68f9f3
Imran <martanec@bk.ru>
2022-02-20 00:45:42 +0300

    добавлена страница MyPage

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,13 @@ import Find from '../pages/Auth/Find'
 import Login from '../pages/Auth/Login'
 import ResetPassword from '../pages/Auth/ResetPassword'
 import SignUp from '../pages/Auth/SignUp'
+import Home from '../pages/HomePage/Index'
+import MyPage from '../pages/MyPage/MyPage'
 
 function App() {
   return (
     <div>
-      <Find />
-      <SignUp />
+
     </div>
   );
 }
--- src/pages/ProductPage/ContentProduct.jsx
+++ src/pages/ProductPage/ContentProduct.jsx
@@ -28,7 +28,7 @@ function ContentProduct (props) {
           <div className='col-4 row justify-content-between'>
             <div className='col text-sm-end p-0'>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                   className="bi bi-chat-square-text text-secondary" viewBox="0 0 16 16">
+                   className="bi bi-chat-square-text text-secondary mb-1" viewBox="0 0 16 16">
                 <path
                   d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                 <path
@@ -40,7 +40,7 @@ function ContentProduct (props) {
             </div>
             <div className='col text-center p-0'>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                   className="bi bi-arrow-down-circle text-secondary" viewBox="0 0 16 16">
+                   className="bi bi-arrow-down-circle text-secondary mb-1" viewBox="0 0 16 16">
                 <path fill-rule="evenodd"
                       d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
               </svg>
